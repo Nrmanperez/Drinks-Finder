b1294996fd25dbfadfabc494ee67a00bed8e3242
Nrmanperez <nperez@nicarao.agency>
2022-08-16 09:02:44 -0600

    Added responsive and random images

--- src/components/sections/Cards.jsx
+++ src/components/sections/Cards.jsx
@@ -10,68 +10,60 @@ import {
   ModalFooter,
   ModalBody,
   ModalCloseButton,
-  useDisclosure
+  useDisclosure,
+  Text,
 } from "@chakra-ui/react";
 
-export default function Cards({drink}) {
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    console.log(drink)
-    const property = {
-      imageUrl: drink.strDrinkThumb,
-      imageAlt: 'Rear view of modern home with pool',
-      title: drink.strDrink,
-    }
-  
-    return (
-      <Box maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
+export default function Cards({ drink }) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  console.log(drink);
+  const property = {
+    imageUrl: drink.strDrinkThumb,
+    imageAlt: "Rear view of modern home with pool",
+    title: drink.strDrink,
+  };
+
+  return (
+    <>
+      <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
         <Image src={property.imageUrl} alt={property.imageAlt} />
-  
-        <Box p='6'>
+
+        <Box p="6">
           <Box
-            mt='1'
-            fontWeight='semibold'
-            as='h4'
-            lineHeight='tight'
+            mt="1"
+            fontWeight="semibold"
+            as="h4"
+            lineHeight="tight"
             noOfLines={1}
           >
             {property.title}
           </Box>
-  
-          <Box display='flex' mt='2' alignItems='center'>
-          <Button onClick={Modal}>Open Modal</Button>
+
+          <Box display="flex" mt="2" alignItems="center">
+            <Button onClick={onOpen}>Open Modal</Button>
           </Box>
         </Box>
       </Box>
-    )
 
-    function Modal() {
-        const { isOpen, onOpen, onClose } = useDisclosure()
-      
-        return (
-          <>
-            <Button onClick={onOpen}>Open Modal</Button>
-      
-            <Modal blockScrollOnMount={false} isOpen={isOpen} onClose={onClose}>
-              <ModalOverlay />
-              <ModalContent>
-                <ModalHeader>Modal Title</ModalHeader>
-                <ModalCloseButton />
-                <ModalBody>
-                  <Text fontWeight='bold' mb='1rem'>
-                    You can scroll the content behind the modal
-                  </Text>
-                  <Lorem count={2} />
-                </ModalBody>
-      
-                <ModalFooter>
-                  <Button colorScheme='blue' mr={3} onClick={onClose}>
-                    Close
-                  </Button>
-                  <Button variant='ghost'>Secondary Action</Button>
-                </ModalFooter>
-              </ModalContent>
-            </Modal>
-          </>
-        )
-      }
-  }
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text fontWeight="bold" mb="1rem">
+              You can scroll the content behind the modal
+            </Text>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost">Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
--- src/pages/HookForm.jsx
+++ src/pages/HookForm.jsx
@@ -1,7 +1,7 @@
 import useCategorys from "../hooks/useCategorys";
 import { useFormik } from "formik";
-import axios from 'axios'
-import { useState } from 'react'
+import axios from "axios";
+import { useEffect, useState } from "react";
 import {
   FormControl,
   Input,
@@ -11,13 +11,14 @@ import {
   Flex,
   FormErrorMessage,
   Box,
+  HStack,
 } from "@chakra-ui/react";
 import Cards from "../components/sections/Cards";
-import * as Yup from 'yup'
+import * as Yup from "yup";
 
 export default function HookForm() {
   const { categorys } = useCategorys();
-  const [drinks, setDrinks] = useState([])
+  const [drinks, setDrinks] = useState([]);
 
   const formik = useFormik({
     initialValues: {
@@ -25,31 +26,53 @@ export default function HookForm() {
       category: "",
     },
     validationSchema: Yup.object({
-      name: Yup.string().required('Name is required'),
-      category: Yup.string().required('Category is required')
+      name: Yup.string().required("Name is required"),
+      category: Yup.string().required("Category is required"),
     }),
     onSubmit: async (values, actions) => {
       try {
-        const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${values.name}&c=${values.category}`
+        const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${values.name}&c=${values.category}`;
 
-        const { data } = await axios(url)
-        const { drinks } = data
-        setDrinks(drinks)
-    } catch (error) {
-        console.log(error)
-    }
-      actions.resetForm()
-    }
+        const { data } = await axios(url);
+        const { drinks } = data;
+        setDrinks(drinks);
+      } catch (error) {
+        console.log(error);
+      }
+      actions.resetForm();
+    },
   });
 
+  const random = async () => {
+    try {
+      const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=margarita&c=Cocktail`;
+
+      const { data } = await axios(url);
+      const { drinks } = data;
+      setDrinks(drinks);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    random();
+  }, []);
+
   return (
-    <Container minH="78.5vh">
-      <form onSubmit={formik.handleSubmit}>
-        <Flex gap="2" direction={{ base: "column", md: "row" }} minWidth='max-content' alignItems='center'>
-          <FormControl isInvalid={formik.errors.name && formik.touched.name}>
-              <Input 
-                placeholder="Name of drink" 
-                borderColor="blue.700" 
+    <>
+      <Container>
+        <form onSubmit={formik.handleSubmit}>
+          <Flex
+            gap="2"
+            direction={{ base: "column", md: "row" }}
+            minWidth="max-content"
+            alignItems="center"
+          >
+            <FormControl isInvalid={formik.errors.name && formik.touched.name}>
+              <Input
+                placeholder="Name of drink"
+                borderColor="blue.700"
                 name="name"
                 type="text"
                 variant="filled"
@@ -58,43 +81,44 @@ export default function HookForm() {
                 onBlur={formik.handleBlur}
               />
               <FormErrorMessage>{formik.errors.name}</FormErrorMessage>
-          </FormControl>
-          <FormControl isInvalid={formik.errors.category && formik.touched.category}>
-            <Select
-              id="category"
-              placeholder="Select Category"
-              name="category"
-              borderColor="blue.700"
-              onChange={formik.handleChange}
-              value={formik.values.search}
-              onBlur={formik.handleBlur}
+            </FormControl>
+            <FormControl
+              isInvalid={formik.errors.category && formik.touched.category}
             >
-              {categorys.map((categoria) => (
-                <option
-                  key={categoria.strCategory}
-                  value={categoria.strCategory}
-                >
-                  {categoria.strCategory}
-                </option>
-              ))}
-            </Select>
-            <FormErrorMessage>{formik.errors.category}</FormErrorMessage>
-          </FormControl>
-          <Button type="submit" colorScheme='teal' width="full">
-            Search Drink
-          </Button>
-        </Flex>
-      </form>
+              <Select
+                id="category"
+                placeholder="Select Category"
+                name="category"
+                borderColor="blue.700"
+                onChange={formik.handleChange}
+                value={formik.values.search}
+                onBlur={formik.handleBlur}
+              >
+                {categorys.map((categoria) => (
+                  <option
+                    key={categoria.strCategory}
+                    value={categoria.strCategory}
+                  >
+                    {categoria.strCategory}
+                  </option>
+                ))}
+              </Select>
+              <FormErrorMessage>{formik.errors.category}</FormErrorMessage>
+            </FormControl>
+            <Button type="submit" colorScheme="teal" width="full">
+              Search Drink
+            </Button>
+          </Flex>
+        </form>
+      </Container>
 
-        <Box w='100%' p={4} mt={4} color='black'>
-          { drinks.map(drink => (
-              <Cards 
-                key={drink.idDrink}
-                drink={drink}
-              />
+      <HStack w="100vw" p={4} mt={4} color="black">
+        <Flex wrap="wrap" justifyContent="space-evenly">
+          {drinks.map((drink) => (
+            <Cards key={drink.idDrink} drink={drink} />
           ))}
-        </Box>
-
-    </Container>
+        </Flex>
+      </HStack>
+    </>
   );
 }
